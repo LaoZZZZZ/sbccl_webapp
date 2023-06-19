e564218a6692a507c6694c53f3e02e4f22d198c2
HAHA <luzhao1986@gmail.com>
2023-06-19 11:39:41 -0400

    Add student model and views.

--- students/views.py
+++ students/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.template import loader
+from models import Student
 
 def students(request):
     template = loader.get_template('login.html')
     return HttpResponse(template.render())
+
+def single_students(request, user_name, password):
+    student = Student.objects.get(user_name=user_name, password=password)
+    
