80385f6c72ae48d4d521760169d757fb70146a50
HAHA <luzhao1986@gmail.com>
2023-06-19 20:21:17 -0400

    Add login/signup/reset password page for students app.

--- students/urls.py
+++ students/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('students/', views.students, name='students'),
+    path('student/', views.student, name='student'),
+    path('student/login/', views.login, name='login'),
+    path('student/details/', views.details, name='details'),
+    path('student/sign_up/', views.sign_up, name='sign-up'),
+    path('student/reset_password/', views.reset_password, name='reset-password'),
 ]
