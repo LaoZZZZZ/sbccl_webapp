5ec2d5c23ccb9afc41ed7ce11baeee2ca5c4317e
HAHA <luzhao1986@gmail.com>
2023-07-02 11:25:57 -0400

    Add boardmember data models and basic email sending library support.
    The service letter generation module is half-baked.

---
+++ board_members/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class BoardMember(models.Model):
+    id = models.IntegerField(primary_key=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    middle_name = models.CharField(max_length=255, null=True)
+    email = models.CharField(max_length=255)
+    recovery_email = models.CharField(max_length=255)
+    join_date = models.DateField(null=True)
+    password = models.CharField(max_length=255)
+    phone_number = models.IntegerField(null=True)
+    user_name = models.CharField(max_length=255, null=True)
+    GENDER_INFO = [
+        ('M', 'Male'),
+        ('F', 'Femail'),
+        ('U', 'Unknown')]
+    gender = models.CharField(max_length=1, choices=GENDER_INFO)
+    # include both first and last name.
+    chinese_name = models.CharField(max_length=255, null=True)
+    JOB_STATUS = [
+        ('A', 'Active'),
+        ('L', 'Leave'),
+        ('U', 'Unknown')
+    ]
+    job_status = models.CharField(max_length=1, choices=GENDER_INFO)
+    job_description = models.CharField(max_length=255, null=True)
+
---
+++ utils/letter_sender.py
@@ -0,0 +1,14 @@
+from email_sender import EmailSender
+from service_letter_generator import ServiceLetter
+
+class LetterSender(object):
+    def __init__(self):
+        pass
+
+    def SendLetter(self):
+        pass
+
+    def SaveLetter(self):
+        pass
+
+if __name__ == '__main__':
