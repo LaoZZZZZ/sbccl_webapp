f5069491b846c0913bf2292741b51e835ea200a0
HAHA <luzhao1986@gmail.com>
2023-07-03 23:30:28 -0400

    Add user app and move student as part of user profile.

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('student/', views.student, name='student'),
+    path('user/', views.login, name='student'),
     path('student/login/', views.login, name='login'),
     path('student/details/', views.details, name='details'),
     path('student/sign_up/', views.sign_up, name='sign-up'),
--- user/views.py
+++ user/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+from . import models
+   
+def login(request):
+    template = loader.get_template('login.html')
+    return HttpResponse(template.render())
 
-# Create your views here.
+def details(request, user_name, password):
+    user = models.User.objects.get(user_name=user_name)
+    if user is None:
+        return HttpResponse()
+    if user.password != password:
+        template = loader.get_template('not_found.html')
+        return HttpResponse()
+    students = models.
+    template = loader.get_template('details.html')
+    return HttpResponse(template.render())
+
+# TODO(Lu): Fill in the implementation
+def sign_up(request, user_name, password):
+    pass
+
+# TODO(Lu): Fill in the implementation
+def reset_password(request, user_name, recovery_email):
+    pass
+
+# Add a student 
+def add_student(request, student_info):
+    pass
+
+# add a student to a course
+def register_student(request, student_id, course_id):
+    pass
+
+# Remove a student from a course
+def unregister_student(request, student_id, course_id):
+    pass
