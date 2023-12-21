dbf404f2baf6bf57a36a90c9dfcd4df857487047
LaoZZZZZ <luzhao1986@gmail.com>
2023-12-21 18:14:09 -0500

    Refactor the login/reset/signup page into a separate component from the App component.

--- frontend/src/components/app/App.tsx
+++ frontend/src/components/app/App.tsx
@@ -6,88 +6,29 @@ import React, {
   useState,
 } from "react";
 
-import LoginPage from "../user/LoginPage.tsx";
 import UserMainPage from "../user/UserMainPage.tsx";
-import SignUpPage from "../user/SignUpPage.tsx";
-import ResetPasswordPage from "../user/ResetPasswordPage.tsx";
-import axios from "axios";
-import Alert from "../common/Alert.tsx";
+import UserFrontPage from "../user/UserFrontPage.tsx";
 // Some Hardcoded user information for development purpose. Read data will be loaded from the
 // backend.
 
 const Page = {
   StartLogin: 0, // User is on login page
-  StartResetPassword: 2, // User is on reset password page
-  StartSignUp: 3, // user is on the sign up page
-  PostLogin: 4, // User successfully logged in, showing the user profile page.
-};
-
-const UserActions = {
-  Login: 0, // User is attempting to login with entered cridential.
-  SignUp: 1, // User is attempting to sign up with entered information
-  ResetPassword: 2, // User is attempting to reset password with new password.
-  Logout: 3, // User is attempting to logout.
-};
-
-const userLogout = async (user_state) => {
-  await axios
-    .put("http://localhost:8000/rest_api/members/logout", {
-      user_state,
-    })
-    .then(function (response) {
-      console.log(response);
-      return {
-        page: Page.StartLogin,
-      };
-    });
-};
-
-const userResetPassword = async (user_state) => {
-  const userLoginResponse = await axios
-    .put("http://localhost:8000/rest_api/members/reset-password", {
-      user_state,
-    })
-    .then(function (response) {
-      console.log(response);
-      if (response.status == 200) {
-        return {
-          page: Page.PostLogin,
-          // Add students here.
-        };
-      } else {
-        return {
-          page: Page.StartResetPassword,
-          // Add students here.
-        };
-      }
-    });
+  PostLogin: 1, // User successfully logged in, showing the user profile page.
 };
 
 const reducer = (user_repo, action) => {
   switch (action.type) {
-    case "login":
-      console.log(user_repo.user_info);
+    case "login_complete":
+      console.log(action.user_info);
+      return {
+        ...user_repo,
+        page: Page.PostLogin,
+        user_info: action.user_info,
+      };
+    case "lougout":
       return {
         ...user_repo,
         page: Page.StartLogin,
-        user_info: action.user_info,
-      };
-    case "logout":
-      return {
-        ...user_repo,
-        page: Page.StartLogin,
-        user_info: null,
-      };
-    case "signup":
-      return {
-        ...user_repo,
-        page: Page.StartSignUp,
-        user_info: action.user_info,
-      };
-    case "reset_password":
-      return {
-        ...user_repo,
-        page: Page.StartResetPassword,
         user_info: action.user_info,
       };
     default:
@@ -102,7 +43,6 @@ const INITIAL_STATE = {
   // a. username, password
   // b. Auth token
   user_info: null,
-  student: [],
 };
 
 export const UserContext = React.createContext([]);
@@ -110,39 +50,17 @@ export const UserContext = React.createContext([]);
 const { Provider } = UserContext;
 
 const App = () => {
-  const [user_repo, dispatch] = useReducer(reducer, INITIAL_STATE);
+  const [user_profile, transitionUserState] = useReducer(
+    reducer,
+    INITIAL_STATE
+  );
 
   return (
-    <Provider value={[user_repo, dispatch]}>
+    <Provider value={[user_profile, transitionUserState]}>
       <div className="container-sm">
-        {user_repo.page === Page.StartLogin && (
-          <LoginPage
-            onSignUp={() => {
-              dispatch({ type: "signup" });
-            }}
-            onResetPassword={() => {
-              dispatch({ type: "reset_password" });
-            }}
-          />
-        )}
+        {user_profile.page === Page.StartLogin && <UserFrontPage />}
 
-        {user_repo.page === Page.PostLogin && <UserMainPage />}
-
-        {user_repo.page === Page.StartSignUp && (
-          <SignUpPage
-            onBackToLogin={() => {
-              dispatch({ type: "login" });
-            }}
-          />
-        )}
-
-        {user_repo.page === Page.StartResetPassword && (
-          <ResetPasswordPage
-            onBackToLogin={() => {
-              dispatch({ type: "login" });
-            }}
-          />
-        )}
+        {user_profile.page === Page.PostLogin && <UserMainPage />}
       </div>
     </Provider>
   );
