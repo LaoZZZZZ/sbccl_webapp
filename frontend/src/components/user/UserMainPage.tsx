f86a11057909d514f5b1a8e8d3a80ccf14712117
LaoZZZZZ <luzhao1986@gmail.com>
2023-12-21 18:14:09 -0500

    Refactor the login/reset/signup page into a separate component from the App component.

--- frontend/src/components/user/UserMainPage.tsx
+++ frontend/src/components/user/UserMainPage.tsx
@@ -1,4 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+// import axios from "axios";
+
+import { UserContext } from "../app/App.tsx";
+
+// const userLogout = async (user_state) => {
+//   await axios
+//     .put("http://localhost:8000/rest_api/members/logout", {
+//       user_state,
+//     })
+//     .then(function (response) {
+//       console.log(response);
+//       return {
+//         page: Page.StartLogin,
+//       };
+//     });
+// };
 
 // Main page for user profile after login.
 // Contains the following components:
@@ -6,7 +22,9 @@ import React from "react";
 // 2. Students under this user
 // 3. Registrations associated with each student
 const UserMainPage = () => {
-  return <div>Details</div>;
+  const [user_profile, transition] = useContext(UserContext);
+
+  return <div>Hello, {JSON.stringify(user_profile.user_info)}</div>;
 };
 
 export default UserMainPage;
