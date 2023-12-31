17ffc222d5e022d264d102c1d2958922571f8092
LaoZZZZZ <luzhao1986@gmail.com>
2023-12-31 11:02:48 -0500

    Connect to postgresql database.

--- sbccl_webapp/settings.py
+++ sbccl_webapp/settings.py
@@ -114,10 +114,13 @@ WSGI_APPLICATION = 'sbccl_webapp.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "sbccl",
+            "passfile": "/Users/luzhao/my_pgpass",
+        },
+    },
 }
 
 
