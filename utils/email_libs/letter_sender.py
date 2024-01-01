37a36c2a519538868eba9e457ad79d65e735624f
LaoZZZZZ <luzhao1986@gmail.com>
2024-01-01 13:32:14 -0500

    Resture utils folder.

---
+++ utils/email_libs/letter_sender.py
@@ -0,0 +1,15 @@
+from email_sender import EmailSender
+from service_letter_generator import ServiceLetter
+
+# A class send an email with attached documents (mostly a PDF).
+class LetterSender(object):
+    def __init__(self):
+        pass
+
+    def SendLetter(self):
+        pass
+
+    def SaveLetter(self):
+        pass
+
+if __name__ == '__main__':
