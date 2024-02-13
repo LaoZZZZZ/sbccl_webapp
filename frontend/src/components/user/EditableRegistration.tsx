ea3ed7176c9d8ea11bb918e5947fbb8768db1cef
LaoZZZZZ <luzhao1986@gmail.com>
2024-02-13 17:16:07 -0500

    Add TODO and dummy registration page.

--- frontend/src/components/user/EditableRegistration.tsx
+++ frontend/src/components/user/EditableRegistration.tsx
@@ -21,7 +21,6 @@ interface Student {
 interface Registration {
   course_id: string;
   student: Student;
-  pod: string;
 }
 
 const AddStatus = {
@@ -66,15 +65,6 @@ const AddRegistrationRequest = async (registration, authInfo, callBack) => {
     });
 };
 
-interface ClassInformation {
-  selected: boolean;
-  enrollment: number;
-  capacity: number;
-  teacher: string;
-  cost: string;
-  type: string;
-}
-
 const AddRegistration = ({
   userAuth,
   students,
@@ -82,28 +72,7 @@ const AddRegistration = ({
   updateRegistrationList,
   cancelCallback,
 }: Props) => {
-  // TODO(lu): Read the available dates from the backend.
-  const podDates = [
-    "2024-01-24 10:00am",
-    "2024-01-24 01:00pm",
-    "2024-02-24 10:00am",
-    "2024-02-24 01:00pm",
-    "2024-03-24 10:00am",
-    "2024-04-24 10:00am",
-    "2024-05-24 10:00am",
-    "2024-06-24 10:00am",
-    "2024-07-24 10:00am",
-  ];
   const [addStatus, setAddStatus] = useState({});
-  const [classInfo, setClassInfo] = useState<ClassInformation>({
-    selected: false,
-    enrollment: 0,
-    capacity: 0,
-    teacher: "",
-    cost: "",
-    type: "",
-  });
-
   const [registration] = useState<Registration>({
     course_id: "",
     student: {
@@ -112,13 +81,20 @@ const AddRegistration = ({
       gender: "",
       date_of_birth: new Date("1997-01-01"),
     },
-    pod: "",
   });
 
   return (
     <div className="col w-75 mx-auto align-middle">
       <form className="form-label form-control">
-        <div className="form-group pb-2">
+        <input
+          className="form-control"
+          type="text"
+          placeholder="Disabled input"
+          aria-label="Disabled input example"
+          disabled
+        />
+
+        <div className="form-group">
           <label>Select student</label>
           <select
             className="form-control"
@@ -148,25 +124,8 @@ const AddRegistration = ({
               const selected_course = courses.filter(
                 (course) => e.target.value === course.name
               );
-              if (selected_course.length === 1) {
+              if (selected_course.length === 1)
                 registration.course_id = selected_course[0].id;
-                setClassInfo({
-                  selected: true,
-                  enrollment: selected_course[0].enrollment,
-                  capacity: selected_course[0].size_limit,
-                  cost: "$" + selected_course[0].cost,
-                  type: selected_course[0].course_type,
-                  teacher: "***",
-                });
-              } else {
-                setClassInfo({
-                  selected: false,
-                  enrollment: 0,
-                  capacity: 0,
-                  cost: 0,
-                  teacher: "",
-                });
-              }
             }}
           >
             <option>Not Selected</option>
@@ -176,57 +135,11 @@ const AddRegistration = ({
             })}
           </select>
         </div>
-        {classInfo.selected && (
-          <div className="input-group pb-2">
-            <span className="input-group-text">Capacity:</span>
-            <span className="input-group-text bg-white">
-              {classInfo.capacity}
-            </span>
-            <span className="input-group-text">Enrollment:</span>
-            <span
-              className={
-                "input-group-text " +
-                (classInfo.enrollment >= classInfo.capacity
-                  ? "bg-danger"
-                  : "bg-white")
-              }
-            >
-              {classInfo.enrollment}
-            </span>
-            <span className="input-group-text">Cost</span>
-            <span className="input-group-text bg-white">{classInfo.cost}</span>
-            <span className="input-group-text">Teacher</span>
-            <span className="input-group-text bg-white">
-              {classInfo.teacher}
-            </span>
-          </div>
-        )}
-        {classInfo.selected && classInfo.type == "L" && (
-          <div className="form-group pb-2">
-            <label>Select Parent On Duty date</label>
-            <select
-              className="form-control"
-              id="podSelect"
-              onChange={(e) => {
-                registration.pod = e.target.value;
-              }}
-            >
-              <option>Not Selected</option>
-              {podDates.map((date) => {
-                return <option>{date}</option>;
-              })}
-            </select>
-          </div>
-        )}
         <div className="btn-group pt-2">
           <input
             className="btn btn-primary active mr-2"
             type="button"
-            value={
-              !classInfo.selected || classInfo.enrollment < classInfo.capacity
-                ? "Register"
-                : "Add to Waiting list"
-            }
+            value="Add"
             onClick={() => {
               console.log(registration);
               AddRegistrationRequest(registration, userAuth, (result) => {
