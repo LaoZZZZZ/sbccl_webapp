87415315a958adea142b01fbb1480a84b8ee6724
LaoZZZZZ <luzhao1986@gmail.com>
2024-03-10 22:23:57 -0400

    use https in remote deployment.

--- frontend/src/components/user/FetchRegistrations.tsx
+++ frontend/src/components/user/FetchRegistrations.tsx
@@ -3,11 +3,15 @@ import axios from "axios";
 const fetchRegistrations = async (user_info, callback) => {
   const response = await axios.get(
 <<<<<<< HEAD
+<<<<<<< HEAD
     process.env.REACT_APP_BE_URL_PREFIX +
 =======
     "http://" +
       process.env.REACT_APP_BE_URL_PREFIX +
 >>>>>>> a0582317 (Parameterize backend hostname.)
+=======
+    process.env.REACT_APP_BE_URL_PREFIX +
+>>>>>>> 4e711736 (use https in remote deployment.)
       "/rest_api/members/list-registrations",
     {
       headers: {
--- frontend/src/components/user/Logout.tsx
+++ frontend/src/components/user/Logout.tsx
@@ -4,12 +4,16 @@ const Logout = async (userInfo, logOutCallback) => {
   await axios
     .put(
 <<<<<<< HEAD
+<<<<<<< HEAD
       process.env.REACT_APP_BE_URL_PREFIX + "/rest_api/members/logout/",
 =======
       "http://" +
         process.env.REACT_APP_BE_URL_PREFIX +
         "/rest_api/members/logout/",
 >>>>>>> a0582317 (Parameterize backend hostname.)
+=======
+      process.env.REACT_APP_BE_URL_PREFIX + "/rest_api/members/logout/",
+>>>>>>> 4e711736 (use https in remote deployment.)
       {},
       {
         auth: userInfo.auth,
