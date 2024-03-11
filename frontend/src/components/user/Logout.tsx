815ecdf1d2fe5d540a0241fa916bbc9999b51ec9
LaoZZZZZ <luzhao1986@gmail.com>
2024-03-10 22:10:32 -0400

    Parameterize backend hostname.

--- frontend/src/components/user/Logout.tsx
+++ frontend/src/components/user/Logout.tsx
@@ -3,17 +3,7 @@ import axios from "axios";
 const Logout = async (userInfo, logOutCallback) => {
   await axios
     .put(
-<<<<<<< HEAD
-<<<<<<< HEAD
       process.env.REACT_APP_BE_URL_PREFIX + "/rest_api/members/logout/",
-=======
-      "http://" +
-        process.env.REACT_APP_BE_URL_PREFIX +
-        "/rest_api/members/logout/",
->>>>>>> a0582317 (Parameterize backend hostname.)
-=======
-      process.env.REACT_APP_BE_URL_PREFIX + "/rest_api/members/logout/",
->>>>>>> 4e711736 (use https in remote deployment.)
       {},
       {
         auth: userInfo.auth,
