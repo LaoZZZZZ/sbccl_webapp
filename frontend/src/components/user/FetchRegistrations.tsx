400ef4077ef4519c031eae69007fdbb1edfa21cc
LaoZZZZZ <luzhao1986@gmail.com>
2024-03-10 22:10:32 -0400

    Parameterize backend hostname.

--- frontend/src/components/user/FetchRegistrations.tsx
+++ frontend/src/components/user/FetchRegistrations.tsx
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const fetchRegistrations = async (user_info, callback) => {
   const response = await axios.get(
-    "http://localhost:8000/rest_api/members/list-registrations",
+    "http://" +
+      process.env.REACT_APP_BE_URL_PREFIX +
+      "/rest_api/members/list-registrations",
     {
       headers: {
         "Content-Type": "application/json",
