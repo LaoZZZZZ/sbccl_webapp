c61732f95a07bc85a5f927243cede7fa79155389
LaoZZZZZ <luzhao1986@gmail.com>
2024-03-16 11:24:40 -0400

    Fix local build error.

--- sbccl_webapp/settings.py
+++ sbccl_webapp/settings.py
@@ -192,7 +192,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 if 'RDS_DB_NAME' in os.environ:
     STATIC_ROOT = '/var/app/current/static/'
 else:
