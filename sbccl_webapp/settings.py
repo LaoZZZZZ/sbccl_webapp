1c04661239a04528182469c7f485f9648cfb49a0
LaoZZZZZ <luzhao1986@gmail.com>
2024-03-16 11:21:41 -0400

    Explicitly specify the /var/app/current in STATIC_ROOT for elastic deployment.

--- sbccl_webapp/settings.py
+++ sbccl_webapp/settings.py
@@ -193,7 +193,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_ROOT = 'static'
+if 'RDS_DB_NAME' in os.environ:
+    STATIC_ROOT = '/var/app/current/static/'
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
