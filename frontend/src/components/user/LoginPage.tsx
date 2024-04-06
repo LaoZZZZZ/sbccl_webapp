72ac548e4044fba9b15aa0a6c7d64da36b09b0b3
LaoZZZZZ <luzhao1986@gmail.com>
2024-04-05 22:25:13 -0400

    Fix the dismissable alert button on the login front page.

--- frontend/src/components/user/LoginPage.tsx
+++ frontend/src/components/user/LoginPage.tsx
@@ -19,83 +19,85 @@ const LoginPage = ({ onLoginSuccess, onSignUp, onResetPassword }: Props) => {
   return (
     <div className="w-50 form-control mx-auto align-middle">
       <form className="control-form">
-        <div className="">
-          <EmailInput parentCallback={setEmailAddress} />
-          <PasswordInput
-            // No need to let the user to confirm their password separately.
-            confirmPassword={false}
-            retrievePassword={setPassword}
-          />
-          <div className="btn-group">
-            <input
-              className="btn btn-primary active"
-              type="button"
-              value="Login"
-              onClick={async () => {
-                if (emailAddress === "" || password === "") {
-                  setLoginFailed(true);
-                  return;
-                }
-                await axios
-                  .put(
-                    process.env.REACT_APP_BE_URL_PREFIX +
-                      "/rest_api/members/login/",
-                    {},
-                    {
+        <EmailInput parentCallback={setEmailAddress} />
+        <PasswordInput
+          // No need to let the user to confirm their password separately.
+          confirmPassword={false}
+          retrievePassword={setPassword}
+        />
+        <div className="btn-group">
+          <input
+            className="btn btn-primary active"
+            type="button"
+            value="Login"
+            onClick={async () => {
+              if (emailAddress === "" || password === "") {
+                setLoginFailed(true);
+                return;
+              }
+              await axios
+                .put(
+                  process.env.REACT_APP_BE_URL_PREFIX +
+                    "/rest_api/members/login/",
+                  {},
+                  {
+                    auth: {
+                      username: emailAddress,
+                      password: password,
+                    },
+                  }
+                )
+                .then(function (response) {
+                  if (response.status === 202) {
+                    const user_data = response.data;
+                    const user_info: UserInfo = {
                       auth: {
-                        username: emailAddress,
                         password: password,
+                        username: user_data.user.username,
                       },
-                    }
-                  )
-                  .then(function (response) {
-                    if (response.status === 202) {
-                      const user_data = response.data;
-                      const user_info: UserInfo = {
-                        auth: {
-                          password: password,
-                          username: user_data.user.username,
-                        },
-                        user: user_data.user,
-                      };
-                      onLoginSuccess(user_info);
-                    }
-                  })
-                  .catch(function (error) {
-                    setLoginFailed(true);
-                    if (error.response.data)
-                      setLoginErrorMsg(error.response.data.detail);
-                  });
-              }}
-            />
-            <input
-              className="btn btn-primary"
-              type="button"
-              value="Sign up"
-              onClick={() => {
-                onSignUp();
-              }}
-            />
-            <input
-              className="btn btn-secondary"
-              type="button"
-              value="Reset password"
-              onClick={() => {
-                onResetPassword();
-              }}
-            />
-          </div>
+                      user: user_data.user,
+                    };
+                    onLoginSuccess(user_info);
+                  }
+                })
+                .catch(function (error) {
+                  if (error.response.data) {
+                    console.log(error.response.data);
+                    setLoginErrorMsg(error.response.data.detail);
+                  }
+                  setLoginFailed(true);
+                });
+            }}
+          />
+          <input
+            className="btn btn-primary"
+            type="button"
+            value="Sign up"
+            onClick={() => {
+              onSignUp();
+            }}
+          />
+          <input
+            className="btn btn-secondary"
+            type="button"
+            value="Reset password"
+            onClick={() => {
+              onResetPassword();
+            }}
+          />
         </div>
       </form>
       {loginFailed && (
-        <Alert
-          success={false}
-          message={loginErrorMsg}
-          parentCallback={() => {
-            setLoginFailed(false);
-            setLoginErrorMsg("");
-          }}
-        />
+        <div>
+          <Alert
+            success={false}
+            message={loginErrorMsg}
+            parentCallback={() => {
+              setLoginErrorMsg("");
+              setLoginFailed(false);
+            }}
+          />
+        </div>
       )}
     </div>
   );
