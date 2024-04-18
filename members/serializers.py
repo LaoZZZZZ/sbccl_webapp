54ce00d80d822b6f45504ea5dd036e02581a3d24
LaoZZZZZ <luzhao1986@gmail.com>
2024-04-17 22:11:36 -0400

    Strip password from teacher information.

--- members/serializers.py
+++ members/serializers.py
@@ -8,7 +8,7 @@ from datetime import datetime
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name')
+        fields = ('username', 'email', 'first_name', 'last_name', 'password')
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
