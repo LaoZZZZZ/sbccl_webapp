92adf46294e292a336306dcd02bb96ee13d33be2
LaoZZZZZ <luzhao1986@gmail.com>
2024-06-29 12:06:12 -0400

    Add an api endpoint that returns a list of volunteer of a course.

--- frontend/src/components/user/CoursesNavigationPage.tsx
+++ frontend/src/components/user/CoursesNavigationPage.tsx
@@ -19,6 +19,7 @@ interface CoursesNavigationPageProps {
 interface Roster {
   fetched: boolean;
   students: RosterStudent[];
+  tas: [];
 }
 
 interface CourseList {
@@ -49,6 +50,7 @@ const CoursesNavigationPage = ({
   const [roster, setRoster] = useState<Roster>({
     fetched: false,
     students: [],
+    tas: [],
   });
 
   const [selectedCourse, setSelectedCourse] = useState({
